4efe9e8330edf0b1b3f1a054c4766559b9397265
get-started-tdd <32414154@qq.com>
2016-04-05 15:48:08 +0800

    提取AuctionMessageTranslator对象负责翻译来自拍卖的消息

---
+++ src/main/java/auctionsniper/AuctionEventListener.java
@@ -0,0 +1,10 @@
+package auctionsniper;
+
+import java.util.EventListener;
+
+/**
+ * Created by L.x on 16-4-5.
+ */
+public interface AuctionEventListener extends EventListener {
+    void auctionClosed();
+}
---
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -0,0 +1,21 @@
+package auctionsniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+/**
+ * Created by L.x on 16-4-5.
+ */
+public class AuctionMessageTranslator implements MessageListener {
+    private AuctionEventListener listener;
+
+    public AuctionMessageTranslator(AuctionEventListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void processMessage(Chat chat, Message message) {
+        listener.auctionClosed();
+    }
+}
